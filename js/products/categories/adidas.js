42338b9202f68d0b528773e0855e5babb225ddc5
EREN YILMAZ <tarantinoeren@gmail.com>
2024-03-06 11:48:16 +0300

    first commit

---
+++ js/products/categories/adidas.js
@@ -0,0 +1,111 @@
+/* jshint undef: true, unused: true */
+/* global define: false */
+define([], function(){
+	return {
+		name: "Adidas",
+		oneRow: false,
+		imagesPath: "./images/urun/adidas/",
+		addCodePath: true,
+		items: [
+			{
+				name: "Adidas Campus 00s <br> 'Black'",
+				price: {
+					old: "1879.00",
+					current: "1599.00"
+				},
+				code: "Campus00sBlack",
+				details: "Orjinal, ithal ürün.",
+				thumb: "adidas-campus00sBlack.jpeg",
+				gallery: ["adidas-campus00sBlack.jpeg", "adidas-campus00sBlack2.jpeg","adidas-campus00sBlack3.jpeg"]
+			},
+			{
+				name: "Adidas Campus 00s <br>'Dark Green Gum'",
+				price: {
+					old:"2189.00",
+					current: "1589.00"
+				},
+				code: "Campus00sGreen",
+				details: "Orjinal, ithal ürün.",
+				thumb: "adidas-campus00sGreen.jpeg",
+				gallery: ["adidas-campus00sGreen.jpeg", "adidas-campus00sGreen2.jpeg", "adidas-campus00sGreen3.jpeg", "adidas-campus00sGreen1.jpeg"]
+			},
+			{
+				name: "Adidas Campus 00S <br>'Bark'",
+				price: {
+					current: "1549.00",
+					old: "1897.00"
+				},
+				code: "Campus00sBrown",
+				details: "Orjinal, ithal ürün",
+				thumb: "adidas-campus00sBrown.jpeg",
+				gallery: ["adidas-campus00sBrown.jpeg", "adidas-campus00sBrown1.jpeg", "adidas-campus00sBrown2.jpeg", "adidas-campus00sBrown3.jpeg","adidas-campus00sBrown4"]
+			},
+			{
+				name: "Adidas Campus 00s <br>'Better Scarlet Clear Pink'",
+				price: {
+					current: "1549.00",
+					old: "2190.00"
+				},
+				code: "Campus00sPink",
+				details: "Orjinal, ithal ürün.",
+				thumb: "adidas-campus00sPink2.jpeg",
+				gallery: ["adidas-campus00sPink.jpeg", "adidas-campus00sPink2.jpeg", "adidas-campus00sPink3.jpeg","adidas-campus00sPink4.jpeg"],
+			},
+			{
+				name: "Adidas Campus 00s <br>'Crystal White Dark Green'",
+				price: {
+					current: "1589.00",
+					old: "1889.00"
+				},
+				code: "Campus00sLime",
+				details: "Orjinal, ithal ürün.",
+				thumb: "adidas-campus00sLime.jpeg",
+				gallery: ["adidas-campus00sLime.jpeg", "adidas-campus00sLime2.jpeg", "adidas-campus00sLime3.jpeg","adidas-campus00sLime4.jpeg"]
+			},
+			{
+				name: "Adidas Superstar <br>'All White'",
+				price: {
+					old: "1099.00",
+					current: "889.00"
+				},
+				code: "SuperStarAllWhite",
+				details: "Orjinal, ithal ürün.",
+				thumb: "superstarallwhite.jpg",
+				gallery: ["superstarallwhite.jpg"]
+			},
+			{
+				name: "Adidas Bad Bunny <br>'Pink'",
+				price: {
+					current: "1389.00",
+					old: "2100.00"
+				},
+				code: "BadPunnyPink",
+				details: "Orjinal, ithal ürün.",
+				thumb: "badpunny.jpeg",
+				gallery: ["badpunny.jpeg", "badpunny2.jpeg", "badpunny3.jpeg"]
+			},
+			{
+				name: "Adidas Originals Forum Low White Royal",
+				price: {
+					current: "1649.00",
+					old: "2299.00"
+				},
+				code: "ForumBlue",
+				details: "Orjinal, ithal ürün.",
+				thumb: "forumblue5.jpeg",
+				gallery: ["forumblue.jpeg", "forumblue5.jpeg", "forumblue3.jpeg","forumblue2.jpeg","forumblue4.jpeg"]
+			},
+			{
+				name: "Adidas Bad Bunny x Forum Buckle Low Brown",
+				price: {
+					current: "1689.00",
+					old: "2490.00"
+				},
+				code: "BadBunnyBrown",
+				details: "Orjinal, ithal ürün.",
+				thumb: "1.jpeg",
+				gallery: ["1.jpeg", "2.jpeg", "3.jpeg","4.jpeg"]
+			}
+		]
+	};
+});
---
+++ js/products/categories/nike.js
@@ -0,0 +1,100 @@
+/* jshint undef: true, unused: true */
+/* global define: false */
+define([], function(){
+	return {
+		name: "Nike",
+		oneRow: false,
+		imagesPath: "./images/urun/nike/",
+		addCodePath: true,
+		items: [
+			{
+				name: "Nike Air Force <br>White",
+				price: {
+					old:"1879.00",
+					current: "1239.00"
+				},
+				code: "AirForceWhite",
+				details: "Orjinal, ithal ürün.",
+				thumb: "1.jpeg",
+				gallery: ["1.jpeg"]
+			},
+			{
+				name: "Nike Court Vision<br>White Black",
+				price: {
+					current: "879.00",
+					old: "1489.00"
+				},
+				code: "CourtVisionBlack",
+				details: "Orjinal, ithal ürün.",
+				thumb: "1.jpeg",
+				gallery: ["1.jpeg", "2.jpeg", "3.jpeg"]
+			},
+			{
+				name: "Nike Court Vision<br>White White",
+				price: {
+					current: "889.00",
+					old: "1489.00"
+				},
+				code: "CourtVisionWhite",
+				details: "Orjinal, ithal ürün.",
+				thumb: "1.jpeg",
+				gallery: ["1.jpeg", "2.jpeg", "3.jpeg"]
+			},
+			{
+				name: "Nike Dung Low Grey",
+				price: {
+					old: "1489.00",
+					current: "1039.00"
+				},
+				code: "DunkLowGrey",
+				details: "Orjinal, ithal ürün.",
+				thumb: "1.jpeg",
+				gallery: ["1.jpeg", "2.jpeg", "3.jpeg"]
+			},
+			{
+				name: "Nike Dunk <br> Panda",
+				price: {
+					current: "1089.00",
+					old: "1749.00"
+				},
+				code: "DunkPanda",
+				details: "Orjinal, ithal ürün.",
+				thumb: "1.jpeg",
+				gallery: ["1.jpeg", "2.jpeg", "3.jpeg"]
+			},
+			{
+				name: "Air Jordan 1 Retro High <br>OG Bloodline",
+				price: {
+					current: "1719.00",
+					old:"2099.00"
+				},
+				code: "JordanOgBloodline",
+				details: "Orjinal, ithal ürün.",
+				thumb: "1.jpeg",
+				gallery: ["1.jpeg", "2.jpeg", "3.jpeg","4.jpeg","5.jpeg"]
+			},
+			{
+				name: "Air Jordan 1 Retro High <br>OG Voodoo",
+				price: {
+					current: "1679.00",
+					old: "2249.00"
+				},
+				code: "JordanOgVoodoo",
+				details: "Orjinal, ithal ürün.",
+				thumb: "1.jpeg",
+				gallery: ["1.jpeg", "2.jpeg", "3.jpeg","4.jpeg","5.jpeg"]
+			},
+			{
+				name: "Strangelove Skateboards x Nike SB Dunk Low",
+				price: {
+					current: "1789.00",
+					old:"2249.00"
+				},
+				code: "DunkPink",
+				details: "Orjinal, ithal ürün.",
+				thumb: "1.jpeg",
+				gallery: ["1.jpeg", "4.jpeg", "3.jpeg","2.jpeg","5.jpeg"]
+			}
+		]
+	};
+});
